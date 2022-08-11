a7faa5c9f5971d2ae7feb1fc6dab2495d5f56dc8
Jesus Bota <jbota@vesta.net>
2022-08-10 23:20:01 -0500

    added env variables and dev flavor

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+require('dotenv').config()
 
 // Path publico
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
-app.listen(3000, (error) =>{
+app.listen(process.env.PORT, (error) =>{
     if(error) throw new Error(error);
-    console.log('Servidor corriendo en puerto', 3000)
+    console.log('Servidor corriendo en puerto', process.env.PORT)
 })
