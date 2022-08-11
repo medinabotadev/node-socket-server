69cf599ed06c9325ea316cce813759dc62ae6ad4
Jesus Bota <jbota@vesta.net>
2022-08-11 00:39:52 -0500

    emit and listen evetns

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 require('dotenv').config()
+
+// App de Express
+const app = express();
+
+// Node server (Socket)
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+// Mensajes de sockets
+io.on('connection', client => {
+    console.log('Client connected')
+    client.on('event', data => {
+        console.log('event')
+    });
+    client.on('disconnect', () => {
+        console.log('Client disconnected')
+    });
+
+    client.on('message', (payload) => {
+        console.log('Message receive from backend', payload)
+
+        io.emit('message', {server: 'Message receive'})
+    });
+});
+
 
 // Path publico
 const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT, (error) =>{
-    if(error) throw new Error(error);
-    console.log('Servidor corriendo en puerto', process.env.PORT)
+server.listen(process.env.PORT, (error) => {
+    if (error) throw new Error(error);
+    console.log('Server running on port', process.env.PORT)
 })
